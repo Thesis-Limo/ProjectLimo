c49a626ab504eb940292dac2720968c24326f88e
jarnerius <jarnerius@outlook.com>
2024-05-14 15:06:48 +0200

    fixing memory issues

--- ROS/src/limo_behaviour_tree/src/Conditions/MinDistance.cpp
+++ ROS/src/limo_behaviour_tree/src/Conditions/MinDistance.cpp
@@ -5,7 +5,7 @@ MinDistance::MinDistance(const ros::NodeHandle& nodehandle,const ros::Publisher&
   :Node(nodehandle, logPub)
 
 {
-    sub = nh.subscribe<PointCloud>("/camera/depth/points",1, &MinDistance::CallBackPoints, this);
+    sub = nh.subscribe<sensor_msgs::LaserScan>("/scan",1, &MinDistance::CallBackPoints, this);
     minDistance = nh.param<float>("minDistanceWhenBraking",0.25f);
 }
 NodeStatus MinDistance::Tick()
@@ -17,14 +17,26 @@ NodeStatus MinDistance::Tick()
         return NodeStatus::SUCCESS;
     return NodeStatus::FAILURE;
 }
-void MinDistance::CallBackPoints(const PointCloud::ConstPtr&  msg)
+void MinDistance::CallBackPoints(const sensor_msgs::LaserScan::ConstPtr&  laser)
 /*
  * checks every pixel from the depth camera and looks for closest distance
 */
-{  
+{ 
+    float min = 0.61;
     float minDistance = __FLT_MAX__;
-    BOOST_FOREACH (const pcl::PointXYZ& pt, msg->points){
-        if(minDistance > pt.z) minDistance = pt.z;
+
+    float min_dist = laser->range_min;
+    float max_dist = laser->range_max;
+    int startId = 0;
+    for (int i = 0; i < laser->ranges.size(); i++)
+    {
+        float distance = laser->ranges[i];
+        if(min_dist < distance && max_dist > distance)
+        {
+            float angle = laser->angle_min + i * laser->angle_increment;
+            if (abs(angle) > min) continue;
+            if(minDistance > distance) minDistance = currentDistance;
+        }
     }
     currentDistance = minDistance;
 }
