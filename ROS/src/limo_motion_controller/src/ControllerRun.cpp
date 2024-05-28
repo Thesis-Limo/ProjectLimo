7c46c9aa563aa1f91ece6e8c566ff7ed4b384eb0
jarnerius <jarnerius@outlook.com>
2024-05-28 19:24:04 +0200

    change speed to 100 a second for the controller

--- ROS/src/limo_motion_controller/src/ControllerRun.cpp
+++ ROS/src/limo_motion_controller/src/ControllerRun.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[])
     ros::init(argc, argv,"limo_motion_controller");
 
     ros::NodeHandle nh("~");
-    ros::Rate r(10); // 10 hz
+    ros::Rate r(100); // 10 hz
     Controller c = Controller(nh);
     while (ros::ok())
     {
