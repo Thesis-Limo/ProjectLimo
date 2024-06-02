cfc8be57b85d8f3300659e2d46a5b5c12642f7a1
jarnerius <jarnerius@outlook.com>
2024-06-02 13:36:41 +0200

    fix behaviourtree

--- ROS/src/limo_behaviour_tree/include/Actions/CreatePath.h
+++ ROS/src/limo_behaviour_tree/include/Actions/CreatePath.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <limo_motion_controller/OverrideMotion.h>
 #include "position.hpp"
+#include <geometry_msgs/Twist.h>
 #include <Node.h>
 using namespace BehaviourTree;
 
@@ -7,6 +9,7 @@ class CreatePath: public Node
 {
 private:
   ros::ServiceClient client;
+  ros::ServiceClient controllerClient;
 
 public:
     CreatePath(const ros::NodeHandle& nodehandle, const ros::Publisher& logPub);
