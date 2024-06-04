aa7e8a0e6bb5258dd585ffde1e3fd8e017874ab6
jarnerius <jarnerius@outlook.com>
2024-06-04 20:03:05 +0200

    fix BT

--- ROS/src/limo_behaviour_tree/include/Conditions/CheckPath.h
+++ ROS/src/limo_behaviour_tree/include/Conditions/CheckPath.h
@@ -1,11 +1,14 @@
 #include "position.hpp"
+#include <limo_motion_controller/OverrideMotion.h>
 #include <Node.h>
 using namespace BehaviourTree;
 
 class CheckPath: public Node
 {
 private:
-    ros::ServiceClient pathService;    
+    ros::ServiceClient pathService; 
+  ros::ServiceClient controllerClient;
+
 public:
     CheckPath(const ros::NodeHandle& nodehandle, const ros::Publisher& logPub);
     NodeStatus Tick() override;
--- ROS/src/limo_behaviour_tree/src/Conditions/CloseEnoughToTarget.cpp
+++ ROS/src/limo_behaviour_tree/src/Conditions/CloseEnoughToTarget.cpp
@@ -15,7 +15,7 @@ NodeStatus CloseEnoughToTarget::Tick()
     if(targetPos.x >= __FLT_MAX__ || currentPos.x >= __FLT_MAX__)
         return NodeStatus::FAILURE;
     Point3D toTarget = targetPos - currentPos;
-    if(toTarget.DistanceSqrt() > distanceToClose * distanceToClose)
+    if(toTarget.DistanceSqrt() > distanceToClose * distanceToClose * speedSqr)
     {
         return NodeStatus::SUCCESS;
     }
@@ -31,5 +31,27 @@ void CloseEnoughToTarget::CallBackTarget(const geometry_msgs::Point::ConstPtr& m
 
 void CloseEnoughToTarget::CallBackPosition(const nav_msgs::Odometry::ConstPtr& msg)
 {
+    
     currentPos = Point3D(msg->pose.pose.position);
+    ros::Time currentTime = msg->header.stamp;
+    geometry_msgs::Point currentPos = msg->pose.pose.position;
+    if (prevPos.x >= __FLT_MAX__)
+    {
+        prevPos = currentPos;
+        prevTimestamp = currentTime;
+        return;
+    }
+    geometry_msgs::Point trans = currentPos;
+    trans.x -= prevPos.x;
+    trans.y -= prevPos.y;
+    trans.z -= prevPos.z;
+    double t = (currentTime - prevTimestamp).toSec();
+
+    trans.x /= t;
+    trans.y /= t;
+    trans.z /= t;
+    speedSqr = trans.x*trans.x + trans.y*trans.y + trans.z* trans.z;
+    std::cout <<speedSqr <<"\n";
+    prevPos = currentPos;
+    prevTimestamp = currentTime;
 }
